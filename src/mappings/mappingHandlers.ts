fcc242653e4272271f7cbd10a20b16cc572b494f
MonoGot <kirati.samat@gmail.com>
2022-01-03 14:45:52 +0700

    feat: add StakingReward entity

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,33 +1,29 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {StarterEntity} from "../types";
+import {SubstrateEvent} from "@subql/types";
+import {StakingReward, SumReward} from "../types";
 import {Balance} from "@polkadot/types/interfaces";
 
-
-export async function handleBlock(block: SubstrateBlock): Promise<void> {
-    //Create a new starterEntity with ID using block hash
-    let record = new StarterEntity(block.block.header.hash.toString());
-    //Record block number
-    record.field1 = block.block.header.number.toNumber();
-    await record.save();
+function createSumReward(accountId: string): SumReward {
+  const entity = new SumReward(accountId);
+  entity.totalReward = BigInt(0);
+  return entity;
 }
 
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, balance]}} = event;
-    //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.extrinsic.block.block.header.hash.toString());
-    record.field2 = account.toString();
-    //Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
-    await record.save();
+export async function handleSumRewared(event: SubstrateEvent): Promise<void> {
+  const {event: {data: [account, newReward]}} = event;
+  let entity = await SumReward.get(account.toString());
+  if (entity === undefined) {
+    entity = createSumReward(account.toString());
+  }
+  entity.totalReward = entity.totalReward + (newReward as Balance).toBigInt();
+  entity.blockHeight = event.block.block.header.number.toNumber();
+  await entity.save();
 }
 
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
-    //Date type timestamp
-    record.field4 = extrinsic.block.timestamp;
-    //Boolean tyep
-    record.field5 = true;
-    await record.save();
+export async function handleStakingRewarded(event: SubstrateEvent): Promise<void> {
+  const {event: {data: [account, newReward]}} = event;
+  const entity = new StakingReward(`${event.block.block.header.number}-${event.idx.toString()}`);
+  entity.account = account.toString();
+  entity.balance = (newReward as Balance).toBigInt();
+  entity.date = event.block.timestamp;
+  await entity.save();
 }
-
-
