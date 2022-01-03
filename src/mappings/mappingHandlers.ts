328090bed808ec094b9dda851ff21dd05ad4b1cc
MonoGot <kirati.samat@gmail.com>
2022-01-03 15:58:50 +0700

    feat: support reward

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -8,6 +8,14 @@ function createSumReward(accountId: string): SumReward {
   return entity;
 }
 
+export async function handleSumReward(event: SubstrateEvent): Promise<void> {
+  await handleSumRewared(event);
+}
+
+export async function handleStakingReward(event: SubstrateEvent): Promise<void> {
+  await handleStakingRewarded(event);
+}
+
 export async function handleSumRewared(event: SubstrateEvent): Promise<void> {
   const {event: {data: [account, newReward]}} = event;
   let entity = await SumReward.get(account.toString());
